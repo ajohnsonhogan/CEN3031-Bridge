4f737b56e3c54f5c7b618cf441abbe4114806730
Matthew <mcdermottm@ufl.edu>
2019-03-26 09:42:18 -0400

    prepare for merge

--- server/config/config.js
+++
@@ -1,9 +0,0 @@
-//This file holds any configuration variables we may need 
-//'config.js' is typically ignored by git to protect sensitive information, such as your database's username and password
-
-module.exports = {
-  db: {
-    uri: 'mongodb://user:password1@ds141952.mlab.com:41952/photografly', //place the URI of your mongo database here.
-  }, 
-  port: 8080
-};
