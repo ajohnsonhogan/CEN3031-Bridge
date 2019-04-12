4224686372e3066751def4cd008214144e4af23e
Michael O'Connell <michaeljoconnell@ufl.edu>
2019-04-12 15:37:14 -0400

    fix for real

--- server/config/app.js
+++ server/config/app.js
@@ -1,9 +1,3 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-
-import routes from './server/routes';
-
-
 var config = require('./config'), 
     mongoose = require('mongoose'),   
     express = require('./express');
@@ -14,20 +8,3 @@ module.exports.start = function() {
 	  console.log('App listening on port', this.address().port, app.settings.env);  
     });
 };
-
-const app = express();
-
-// ...
-
-// Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Parse application/json
-app.use(bodyParser.json());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// App routes
-routes(app);
-
-//
