9db1cd059d6a2f6a54b68b2c081a581b3882cf2f
Michael O'Connell <michaeljoconnell@ufl.edu>
2019-04-16 09:03:55 -0400

    IPN development

--- server/routes/orders.server.routes.js
+++ server/routes/orders.server.routes.js
@@ -1,3 +1,4 @@
+import IPNController from './controllers/ipnController.js';
 /* Dependencies */
 var orders = require('../controllers/orders.server.controller.js'), 
     express = require('express'), 
@@ -7,6 +8,17 @@ var orders = require('../controllers/orders.server.controller.js'),
   These method calls are responsible for routing requests to the correct request handler.
   Take note that it is possible for different controller functions to handle requests to the same route.
  */
+ 
+ function routes(app) {
+  // ...
+
+  // PayPal IPN endpoint
+  app.post('/ipn', IPNController.index);
+  
+  // ...
+}
+ 
+ 
  router.route('/getNew')
   .get(orders.listNew);
   
