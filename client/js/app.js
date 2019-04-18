e0d4a7d654a2d01a86fd732b076aaa708f7f092d
sarakinzbruner <36466804+sarakinzbruner@users.noreply.github.com>
2019-04-17 21:49:34 -0400

    checking typos
    hehe

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,6 @@
 /* register the modules the application depends upon here*/
-angular.module('peetree', []);
+angular.module('petree', []);
 angular.module('accounts', []);
 
 /* register the application and inject all the necessary dependencies */
-var app = angular.module('peetreeApp', ['peetree',"accounts"]);
+var app = angular.module('petreeApp', ['petree',"accounts"]);
