7defdd81a7ceb80ee3d7392c1db26a9ead2194d5
sarakinzbruner <36466804+sarakinzbruner@users.noreply.github.com>
2019-04-18 10:41:29 -0400

    mod

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,10 @@
 /* register the modules the application depends upon here*/
 angular.module('petree', []);
+
+/* register the application and inject all the necessary dependencies */
+var app = angular.module('petreeApp', ['petree']);
 angular.module('accounts', []);
 
 /* register the application and inject all the necessary dependencies */
 var app = angular.module('petreeApp', ['petree',"accounts"]);
+
