4ea2a65c24cf0d04d63eead24ff11b47f278462b
Matthew <mcdermottm@ufl.edu>
2019-04-21 13:52:12 -0400

    updated adminID to use cookies

--- client/js/main.js
+++ client/js/main.js
@@ -12,6 +12,34 @@ $('input[name="picSize"]').click(function(e) {
 
 
 
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+var  setCookie = function(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+var adminLogin = function() {
+	var adminID = document.getElementById("inputID").value;
+	if (adminID != "" && adminID != null) {
+	  setCookie("adminID", adminID, 10);
+	}
+	return true;
+}
 
 // When the user clicks on the button, open the modal 
 var showLogin = function() {
